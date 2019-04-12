ff298268be15dbd26914d54d2b77a510063061eb
OscarB <oballesteros@abstract.cl>
2019-04-11 22:56:54 -0400

    add health end point

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,7 @@ end
 RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
-  
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
